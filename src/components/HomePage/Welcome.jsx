d7573e35bdfd4ce93b30d7a16b14dbcbb14bb1eb
Ali-hassan23 <127784171+Ali-hassan23@users.noreply.github.com>
2024-05-09 12:49:01 +0500

    Made some changes in front-end

---
+++ src/components/HomePage/Welcome.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+import Link from "next/link";
+import Image from "next/image";
+
+const Welcome = () => {
+  return (
+    <div className="flex h-full justify-center items-center">
+    <div className="flex flex-row w-full max-w-6xl mx-auto gap-8 items-center justify-center">
+      <div className="flex flex-col w-1/2 gap-5 text-center">
+        <h1 className="text-4xl font-bold">Welcome To SmartStay Lahore</h1> 
+        <p>
+          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Harum
+          aliquid illo libero, nobis vero aut hic distinctio eligendi nisi error
+          architecto impedit! Nisi quam ad ullam molestiae incidunt dolorem
+          quidem?
+        </p>
+      </div>
+      <Image className="rounded-xl" src="/lobby.jpg" alt="HEHE" width={400} height={400}/>
+    </div>
+    </div>
+  );
+};
+
+export default Welcome;
